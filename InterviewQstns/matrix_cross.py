8aa1ab50d39f96bc642cdef6093d6d0c873f6284
Yurii Hriaziev <yuriihriaziev@50-a6-d8-bd-ff-47.local.tld>
2024-09-04 08:47:35 -0400

    matrix cross update

--- InterviewQstns/matrix_cross.py
+++ InterviewQstns/matrix_cross.py
@@ -6,30 +6,36 @@ def solution(matrix):
     cols = len(matrix[0])
     crosses = 0
 
-    # sols = []
+    sols = []
 
     for i in range(rows):
-        # sol = [[]]
         for j in range(cols):
             nbors = []
-            for k in range(1, len(matrix[i])):
+            sol = [[0 for i in range(cols)] for j in range(rows)]
+            for k in range(1, max(len(matrix), len(matrix[i]))):
                 if i - k >= 0:
                     nbors.append(matrix[i-k][j])
+                    sol[i-k][j] = '|'
                 if j + k < len(matrix[i]):
                     nbors.append(matrix[i][j+k])
+                    sol[i][j+k] = '-'
                 if i + k < len(matrix):
                     nbors.append(matrix[i+k][j])
+                    sol[i+k][j] = '|'
                 if j - k >= 0:
                     nbors.append(matrix[i][j-k])
+                    sol[i][j-k] = '-'
             cross = True
             for num in range(len(nbors)-1):
                 if nbors[num] != nbors[num+1]:
                     cross = False
 
             if cross:
+                sol[i][j] = '+'
+                sols.append(sol)
                 crosses += 1
-                # sols.append(sol)
-    return crosses
+
+    return crosses, sols
 
 def static():        
     c1 = [
@@ -66,42 +72,51 @@ def static():
         [1,1,1,1,1],
         [1,1,1,1,1],
     ]
-    # TODO: Fix, something's wrong, shows 1
     c7 = [
         [0, 1, 0, 0, 0],
         [0, 0, 0, 0, 0],
         [1, 0, 0, 1, 1],
         [0, 0, 1, 1, 1],
-        [1, 0, 0, 0, 0]]
+        [1, 0, 0, 0, 0]] 
+    c8 = [
+        [1,1,1,1,1],
+        [1,5,3,1,1],
+        [1,1,1,1,0],
+        [4,1,2,1,1],
+        [5,3,1,1,1]
+    ]
     
-    ans = solution(c7)
-    print(ans)
+    ans, sols = solution(c7)
+    print(f'Total # of crosses {ans}')
 
 
 import random
 from pprint import pprint
+from tabulate import tabulate
 
 def dynamic():
     mn, mx = 5, 11
     size = random.randint(mn, mx), random.randint(mn, mx)
     
-    # vals = ['-', '+']
     # a = [[random.choice(vals) for _ in range(size)] for _ in range(size)]
-    a = [[random.randint(0,1) for _ in range(size[0])] for _ in range(size[1])]
+    grid = [[random.randint(0,1) for _ in range(size[0])] for _ in range(size[1])]
     # pprint(a)
-    sol = solution(a) # TODO: return array of solutions [[], ]
-    # print(sol)
-    return a, sol
+    ans, sols = solution(grid)
+    return grid, ans, sols
 
 if __name__ == '__main__':
     seed = None #42
     random.seed(seed)
     for i in range(100):
-        a, sol = dynamic()
-        if sol:
+        grid, ans, sols = dynamic()
+        if sols:
             print(f"Run: {i} ")
-            pprint(a)
-            print(sol)
+            print('Cross found')
+            pprint(grid)
+            for sol in sols:
+                print(f'{tabulate(sol, tablefmt='plain')}\n')
+            print(f'Total number of crosses {ans}\n\n')
+
 
     # static()
  
